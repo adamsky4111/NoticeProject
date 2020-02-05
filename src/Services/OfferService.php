7cb629c4b503128a3b002b86c2ad6b8dbb177555
adam <adamdz411@gmail.com>
2020-02-05 15:35:05 +0100

    add OfferService

---
+++ src/Services/OfferService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use App\Repository\Interfaces\OfferRepositoryInterface;
+
+class OfferService
+{
+    private $offerRepository;
+
+    public function __construct(OfferRepositoryInterface $offerRepository)
+    {
+        $this->offerRepository = $offerRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->offerRepository->findAll();
+    }
+}
