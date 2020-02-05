2d5ee8aba2466e8898cf033017cb661230768671
adam <adamdz411@gmail.com>
2020-02-05 15:31:03 +0100

    add CustomOfferRepository, RepositoryInterface

---
+++ src/Repository/Custom/OfferRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Repository\Custom;
+
+use App\Entity\Offer;
+use App\Repository\Interfaces\OfferRepositoryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class OfferRepository implements OfferRepositoryInterface
+{
+    private $repository;
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $this->entityManager->getRepository(Offer::class);
+    }
+
+    public function save($offers)
+    {
+        foreach ($offers as $offer){
+            $this->entityManager->persist($offer);
+        }
+        $this->entityManager = flush();
+    }
+
+    public function  findAll()
+    {
+        return $this->repository->findAll();
+    }
+
+
+}
