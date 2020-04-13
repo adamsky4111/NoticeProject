fb43f4754627d8a4d6b290bdf20f4be74a250cbe
adam <adamdz411@gmail.com>
2020-04-14 00:41:07 +0200

    Modify SecurityServiceInterface.php

--- src/Services/Interfaces/SecurityServiceInterface.php
+++ src/Services/Interfaces/SecurityServiceInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Interfaces;
 
+use App\Entity\User;
 use App\Repository\Interfaces\UserRepositoryInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -12,4 +13,14 @@ interface SecurityServiceInterface
     public function saveUser($data);
 
     public function usernameExist($username): bool;
+
+    public function emailExist($email): bool;
+
+    public function getUserByUsername($username): User;
+
+    public function getUserById($id): User;
+
+    public function getUsers(): array;
+
+    public function deleteUser($username = null, $id = null, $email = null);
 }
