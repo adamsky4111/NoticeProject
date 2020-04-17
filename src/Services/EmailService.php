f35b8ef7c0d2ffa97f81d4428034ed484503bcf9
adam <adamdz411@gmail.com>
2020-04-17 19:01:15 +0200

    modify EmailService.php and EmailServiceInterface.php

--- src/Services/EmailService.php
+++ src/Services/EmailService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Services\Interfaces\EmailServiceInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpClient\Exception\JsonException;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
@@ -17,23 +18,12 @@ class EmailService implements EmailServiceInterface
         $this->mailer = $mailer;
     }
 
-    public function sendEmail($data)
+    public function sendEmail(Email $email)
     {
-        $email = $this->createEmail($data);
         try {
             $this->mailer->send($email);
         } catch (TransportExceptionInterface $e) {
             throw new JsonException($e->getMessage());
         }
-    }
-
-    function createEmail($data)
-    {
-        return (new Email())
-            ->from($data['from'])
-            ->to($data['to'])
-            ->subject($data['subject'])
-            ->text($data['text'])
-            ->html($data['htmlBody']);
     }
 }
--- src/Services/Interfaces/EmailServiceInterface.php
+++ src/Services/Interfaces/EmailServiceInterface.php
@@ -3,12 +3,11 @@
 namespace App\Services\Interfaces;
 
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 interface EmailServiceInterface
 {
     public function __construct(MailerInterface $mailer);
 
-    public function sendEmail($data);
-
-    public function createEmail($data);
+    public function sendEmail(Email $email);
 }
