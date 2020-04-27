f3524e3be5a06993d4fef45433053cfd0a96f523
adamsky4111 <adamdz411@gmail.com>
2020-04-28 00:11:05 +0200

    add my notices for account controller

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,9 +6,10 @@ use App\Services\Interfaces\AccountManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/api/account")
@@ -44,14 +45,37 @@ class AccountController extends AbstractController
                 'User account update success',
                 Response::HTTP_OK
             );
-        }
-        else {
+        } else {
             return $this->createResponse(
                 false,
                 'User account update failed',
                 Response::HTTP_NOT_ACCEPTABLE
             );
         }
+    }
+
+    /**
+     * @Route("/my-notices", name="get_my_notices")
+     */
+    public function getMyNotices()
+    {
+        $user = $this->getUser();
+        if ($user instanceof UserInterface && $user->getAccount !== null) {
+            $notices = $user->getAccount->getNotices();
+            $data = [];
+
+            foreach ($notices as $notice) {
+                $data[] = $notice->toArray();
+            }
+
+            return new JsonResponse($data, Response::HTTP_FOUND);
+        }
+
+        return $this->createResponse(
+            false,
+            'Wrong user or no account',
+            Response::HTTP_NOT_ACCEPTABLE
+        );
     }
 
     public function createResponse($status, $message, $code)
